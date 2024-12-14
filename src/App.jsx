ca8d106bdcbb49a31da27863f3db1707bf2da054
Palak <impalak1118@gmail.com>
2024-12-14 22:22:43 +0530

    q

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Background from './components/Background'
-import Foreground from './components/Foreground'
+import Background from './components/Background.jsx'
+import Foreground from './components/Foreground.jsx'
 
 function App() {
   return (
